17d96196177290213f564c0dfa87c7a276f282a5
Patrícia Silva <prrs89@gmail.com>
2025-09-30 11:11:04 +0100

    Install ESLint for Angular
    - ESLint focuses on correctness + best practices
    - npm install --save-dev @eslint/js
    - ng add @angular-eslint/schematics (bridges ESLint with Angular’s ecosystem)
    - Replaces TSLint (deprecated) with ESLint
    - Sets up default configs for .ts + .html
    - Generates an eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,154 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
-const eslintConfigPrettier = require("eslint-config-prettier/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
+// Allows us to bring in the recommended core rules from eslint itself
+const eslint = require('@eslint/js');
+
+// Allows us to use the typed utility for our config, and to bring in the recommended rules for TypeScript projects from typescript-eslint
+const tseslint = require('typescript-eslint');
+
+// Allows us to bring in the recommended rules for Angular projects from angular-eslint
+const angular = require('angular-eslint');
+
+const eslintConfigPrettier = require('eslint-config-prettier/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+// Export our config array, which is composed together thanks to the typed utility function from typescript-eslint
 module.exports = tseslint.config(
   {
-    files: ["**/*.ts"],
+    ignores: ['.angular/**', '.nx/**', 'coverage/**', 'dist/**'],
+    // Everything in this config object targets our TypeScript files (Components, Directives, Pipes, etc.)
+    files: ['**/*.ts'],
     extends: [
+      // Apply the recommended core rules
       eslint.configs.recommended,
+      // Apply the recommended TypeScript rules
       ...tseslint.configs.recommended,
+      // Apply stylistic rules from typescript-eslint that improve code consistency
       ...tseslint.configs.stylistic,
+      // Apply the recommended Angular rules
       ...angular.configs.tsRecommended,
+      // Disable conflicting rules with Prettier
       eslintConfigPrettier,
+      // Prettier as ESLint rule
       eslintPluginPrettierRecommended,
     ],
+    // IMPORTANT: Set the custom processor to enable inline template linting
+    // This allows your inline Component templates to be extracted and linted with the same
+    // rules as your external .html template files
     processor: angular.processInlineTemplates,
+    // Override specific rules for TypeScript files (these will take priority over the extended configs above)
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
+      // Angular selectors
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'pfa',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
+          type: 'element',
+          prefix: 'pfa',
+          style: 'kebab-case',
         },
       ],
+
+      // Angular best practices
+      '@angular-eslint/prefer-on-push-component-change-detection': 'warn',
+      '@angular-eslint/prefer-output-readonly': 'warn',
+      '@angular-eslint/prefer-signals': 'warn',
+      '@angular-eslint/prefer-standalone': 'warn',
+
+      // TypeScript best practices
+      '@typescript-eslint/array-type': ['warn'],
+      '@typescript-eslint/consistent-indexed-object-style': 'off',
+      '@typescript-eslint/consistent-type-assertions': 'warn',
+      '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        {
+          accessibility: 'no-public',
+        },
+      ],
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+        },
+      ],
+      '@typescript-eslint/no-shadow': 'warn',
     },
+
+    // JavaScript best practices
+    eqeqeq: 'error',
+    complexity: ['error', 20],
+    curly: 'error',
+    'guard-for-in': 'error',
+    'max-classes-per-file': ['error', 1],
+    'max-len': [
+      'warn',
+      {
+        code: 120,
+        comments: 160,
+      },
+    ],
+    'max-lines': ['error', 400], // keep files small
+    'no-bitwise': 'error',
+    'no-console': 'off',
+    'no-new-wrappers': 'error',
+    'no-useless-concat': 'error',
+    'no-var': 'error',
+    'no-restricted-syntax': 'off',
+    'no-shadow': 'error',
+    'one-var': ['error', 'never'],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        allowSeparatedGroups: true,
+      },
+    ],
   },
   {
-    files: ["**/*.html"],
+    // Everything in this config object targets our HTML files (both external template files,
+    // AND inline templates thanks to the processor set in the TypeScript config above)
+    files: ['**/*.html'],
     extends: [
+      // Apply the recommended Angular template rules
       ...angular.configs.templateRecommended,
+      // Apply the Angular template rules which focus on accessibility of our apps
       ...angular.configs.templateAccessibility,
     ],
-    rules: {},
-  }
+    rules: {
+      // Angular template best practices
+      '@angular-eslint/template/attributes-order': [
+        'error',
+        {
+          alphabetical: true,
+          order: [
+            'TEMPLATE_REFERENCE', // e.g. `<input #inputRef>`
+            'ATTRIBUTE_BINDING', // e.g. `<input required>`, `id="3"`
+            'INPUT_BINDING', // e.g. `[id]="3"`, `[attr.colspan]="colspan"`,
+            'TWO_WAY_BINDING', // e.g. `[(id)]="id"`,
+            'OUTPUT_BINDING', // e.g. `(idChange)="handleChange()"`,
+          ],
+        },
+      ],
+      '@angular-eslint/template/button-has-type': 'warn',
+      '@angular-eslint/template/cyclomatic-complexity': ['warn', { maxComplexity: 10 }],
+      '@angular-eslint/template/eqeqeq': 'error',
+      '@angular-eslint/template/prefer-control-flow': 'error',
+      '@angular-eslint/template/prefer-ngsrc': 'warn',
+      '@angular-eslint/template/prefer-self-closing-tags': 'warn',
+      '@angular-eslint/template/use-track-by-function': 'warn',
+    },
+  },
 );
