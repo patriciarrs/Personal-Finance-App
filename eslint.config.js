7809cf0232ee2271aa2602ca4d6334606ab800e2
Patrícia Silva <prrs89@gmail.com>
2025-10-05 16:42:47 +0100

    Add Storybook
    npm create storybook@latest

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+// import storybook from "eslint-plugin-storybook";
+
 // @ts-check
 
 // Allows us to bring in the recommended core rules from eslint itself
