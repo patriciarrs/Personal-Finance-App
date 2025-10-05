8f61b794dc87bf2212cea19227d91e0c9d8a49e8
Patrícia Silva <prrs89@gmail.com>
2025-10-05 18:08:42 +0100

    Fix errors on signals usage

--- src/stories/button.component.ts
+++ src/stories/button.component.ts
@@ -32,6 +32,6 @@ export class ButtonComponent {
   get classes(): string[] {
     const mode = this.primary() ? 'storybook-button--primary' : 'storybook-button--secondary';
 
-    return ['storybook-button', `storybook-button--${this.size}`, mode];
+    return ['storybook-button', `storybook-button--${this.size()}`, mode];
   }
 }
