bbb5c13a6760c566e6d39b4d236d5833e9139ba4
Patrícia Silva <prrs89@gmail.com>
2025-10-07 15:45:16 +0100

    Update Stories path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/app/**/*.stories.ts'],
   addons: ['@storybook/addon-docs', '@storybook/addon-onboarding'],
   framework: {
     name: '@storybook/angular',
